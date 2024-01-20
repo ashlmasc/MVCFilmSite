89510906e00c37653402c4d4bd43d48a5baaa8ab
JonathonMcNamara <jonathonmcnamara1234@hotmail.com>
2024-01-20 11:36:57 -0700

    Updated DAOImpl

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,12 +1,18 @@
 package com.skilldistillery.film.data;
 
 import java.sql.SQLException;
+import java.util.List;
 
+import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
 
 public interface FilmDAO {
 	Film findFilmById(int filmId);
 
 	boolean save(Film film);
+
+	Actor findActorById(int actorId);
+
+	List<Film> findFilmsByKeyword(String userKeyword);
 
 }
--- src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
@@ -5,12 +5,13 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
-
 
 @Repository
 public class FilmDAOImpl implements FilmDAO {
@@ -27,7 +28,7 @@ public class FilmDAOImpl implements FilmDAO {
 		} catch (ClassNotFoundException e) {
 			return film;
 		}
-		
+
 		try {
 			Connection connection = DriverManager.getConnection(URL, USER, PWD);
 
@@ -58,17 +59,89 @@ public class FilmDAOImpl implements FilmDAO {
 //			film.setLanguage(filmResult.getString("language_name"));
 //
 //			List<InventoryItem> inventoryItems = findInventoryByFilmId(filmId);
-				//film.setInventoryItems(inventoryItems);
+				// film.setInventoryItems(inventoryItems);
 			}
 			filmResult.close();
 			statement.close();
 			connection.close();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return film;
+	}
+	
+	@Override
+	public List<Film> findFilmsByKeyword(String userKeyword){
+		List<Film> foundFilms = new ArrayList<Film>();
+		String user = "student";
+		String pass = "student";
+		
+		try {
+			Connection conn = DriverManager.getConnection(URL, user, pass);
+
+			String sql = "SELECT * " + " FROM film JOIN language ON film.language_id = language.id "
+					+ " WHERE film.title LIKE ? OR film.description LIKE ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			String userQuery = "%" + userKeyword + "%";
+			ps.setString(1, userQuery);
+			ps.setString(2, userQuery);
+			ResultSet filmResult = ps.executeQuery();
+			while (filmResult.next()) {
+				int id = filmResult.getInt("id");
+				String title = filmResult.getString("title");
+				String desc = filmResult.getString("description");
+				int releaseYear = filmResult.getInt("release_year");
+				int languageId = filmResult.getInt("language_id");
+				int rentalDuration = filmResult.getInt("rental_duration");
+				double rentalRate = filmResult.getDouble("rental_rate");
+				Integer length = filmResult.getInt("length");
+				double replacementCost = filmResult.getDouble("replacement_cost");
+				String rating = filmResult.getString("rating");
+				List<String> categories = filmResult.getString("special_features");
+
+				Film film = new Film(id, title, desc, releaseYear, languageId, rentalDuration, rentalRate, length,
+						replacementCost, rating, categories);
+//				film.setFilmActors(findActorsByFilmId(film.getId()));
+				foundFilms.add(film);
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return foundFilms;
+
+	}
+
+	@Override
+	public Actor findActorById(int actorId) {
+		Actor actor = null;
+		String user = "student";
+		String pass = "student";
+
+		try {
+			Connection conn = DriverManager.getConnection(URL, user, pass);
+
+			String sql = "SELECT * FROM actor WHERE id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, actorId);
+			ResultSet actorResult = ps.executeQuery();
+			if (actorResult.next()) {
+				int id = actorResult.getInt("id");
+				String firstName = actorResult.getString("first_name");
+				String lastName = actorResult.getString("last_name");
+
+				actor = new Actor(id, firstName, lastName);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return actor;
 	}
 
 	@Override
